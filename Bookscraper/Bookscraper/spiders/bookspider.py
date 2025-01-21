3c7b77c97025cb2041bf337832c7bfb82fa85b21
mamun700263 <mamun700263@gmail.com>
2025-01-21 09:06:30 +0600

    created the spider and crawl all the pages

--- Bookscraper/Bookscraper/spiders/bookspider.py
+++ Bookscraper/Bookscraper/spiders/bookspider.py
@@ -7,4 +7,21 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css("article.product_pod")
+        for book in books:
+
+            yield {
+                'image':book.css("div.image_container a").attrib['href'],
+                'name':book.css("h3 a ::text").get(),
+                'book_link':book.css("h3 a").attrib['href']
+            }
+
+        next_page_url = response.css("li.next a").attrib['href']
+        if next_page_url is not None:
+            if 'catalogue/' in next_page_url:
+                next_page_url = "https://books.toscrape.com/"+next_page_url
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/"+next_page_url
+            yield response.follow(next_page_url,callback = self.parse)
+
+
